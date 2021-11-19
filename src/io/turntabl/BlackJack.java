2e1c5a0343817e0c2b4bfc14b23600fa209d3813
geraldelorm <geraldelorm14@gmail.com>
2021-11-19 16:10:53 +0000

    changed Main to BlackJack and improved expections catch block

--- src/io/turntabl/BlackJack.java
+++ src/io/turntabl/BlackJack.java
@@ -1,19 +1,17 @@
 package io.turntabl;
 
-import io.turntabl.main.Card;
-import io.turntabl.main.Deck;
 import io.turntabl.main.Game;
-
-import java.util.Collections;
-import java.util.Scanner;
 
 public class BlackJack {
 
     public static void main(String[] args) {
+//        USING USER INPUT
 //        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
 //        System.out.print("Enter Number Of Players: ");
 //
 //        int numberOfPlayers = myObj.nextInt();  // Read user input
+
+//        USING CLI ARGUMENTS
         try {
             int numberOfPlayers = Integer.parseInt(args[0]);
             Game game = new Game(numberOfPlayers);
